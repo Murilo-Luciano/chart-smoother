6bfa02d4fa8262755d0f1efea329fbe09be95341
Murilo-Luciano <murilo.luciano123@gmail.com>
2023-06-10 14:22:16 -0400

    ts

---
+++ lib/index.d.ts
@@ -0,0 +1,18 @@
+/**
+Smooths a chart by interpolating additional points between existing points.
+@param {number[][]} points - The array of points to be smoothed.
+@param {number} [iterations=1] - The number of smoothing iterations to perform. Defaults to 1.
+@returns {number[][]} - The smoothed array of points.
+@example
+const points = [
+  [0, 0],
+  [2, 2],
+  [3, 3],
+  [4, 1],
+];
+
+const smoothedPoints = chartSmother(points)
+console.log(smoothedPoints)
+// => [ [ 0, 0 ], [ 0.5, 0.5 ], [ 1.5, 1.5 ], [ 2.25, 2.25 ], [ 2.75, 2.75 ], [ 3.25, 2.5 ], [ 3.75, 1.5 ], [ 4, 1 ] ]
+*/
+export declare function chartSmother(points: number[][], iterations?: number): number[][];
